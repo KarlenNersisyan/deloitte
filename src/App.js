0f77a7a4f08c75e3c92e5d5adccc704c23bd83a8
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-25 01:10:23 +0400

    fix(src): changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+// import { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Menu from "./components/Menu/Menu";
+// import { getCategories, getCoreFields } from "./service/service";
+
 function App() {
-  return <h1>Deloitte</h1>;
+  // const [categories, setCategories] = useState("");
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(false);
+
+  // useEffect(() => {
+  //   getCoreFields().then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
+
+  return (
+    <>
+      {/* <Header /> */}
+      {/* <Menu /> */}
+      <List />
+    </>
+  );
 }
 
 export default App;
