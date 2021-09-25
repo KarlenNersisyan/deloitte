7ccb49654dd083db58e21b15482fd0a3f6587257
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-25 12:43:34 +0400

    refactor(*): Added filter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 // import { useEffect, useState } from "react";
-import classNames from "classnames";
+import { useEffect, useMemo, useState } from "react";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Menu from "./components/Menu/Menu";
+import { getAppContents } from "./service/service";
+
 import classes from "./App.module.css";
-// import { getCategories, getCoreFields } from "./service/service";
 
 function App() {
-  // const [categories, setCategories] = useState("");
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
+  const [appContents, setAppContents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  // useEffect(() => {
-  //   getCoreFields().then((res) => {
-  //     console.log(res);
-  //   });
-  // }, []);
+  const [filter, setFilter] = useState("ALL");
+
+  const filteredContent = useMemo(() => {
+    if (filter === "ALL") return appContents;
+    return appContents.filter((content) => content.catName === filter);
+  }, [appContents, filter]);
+
+  useEffect(() => {
+    getAppContents()
+      .then((res) => {
+        setAppContents(res.contents);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
 
   return (
     <>
       <Header />
       <div className={classes.App}>
-        <Menu />
-        <List />
+        <Menu setFilter={setFilter} />
+        <List
+          filteredContent={filteredContent}
+          error={error}
+          loading={loading}
+        />
       </div>
     </>
   );
