aa1039696255e9698c1f607f3cbd53375f365701
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-29 00:02:39 +0400

    feat: Added active style im menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -11,6 +11,8 @@ function Menu({ setFilter, setContentThumbnail, isActive }) {
   const [categories, setCategories] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeGenIndex, setActiveGenIndex] = useState(0);
 
   useEffect(() => {
     getCategories()
@@ -28,23 +30,47 @@ function Menu({ setFilter, setContentThumbnail, isActive }) {
     <div className={cn([classes.menu], { [classes.hide]: !isActive })}>
       <ul className={classes.categoriesMenu}>
         <li
-          className={classes.categoriesItem}
+          className={
+            activeGenIndex === 0
+              ? `${classes.categoriesItem} ${classes.activeGen}`
+              : classes.categoriesItem
+          }
           // onClick={() => setContentThumbnail()}
-          onClick={() => setFilter("ALL")}
+          onClick={() => {
+            setFilter("ALL");
+            setActiveGenIndex(0);
+          }}
         >
           Home
         </li>
-        <li className={classes.categoriesItem} onClick={() => setFilter("ALL")}>
+        <li
+          className={
+            activeGenIndex === 1
+              ? `${classes.categoriesItem} ${classes.activeGen}`
+              : classes.categoriesItem
+          }
+          onClick={() => {
+            setFilter("ALL");
+            setActiveGenIndex(1);
+          }}
+        >
           All
         </li>
         {loading && <Loading />}
         {error && <p>ERROR ...</p>}
         {categories &&
-          categories.map((e) => {
+          categories.map((e, index) => {
             return (
               <li
-                className={classes.categoriesItem}
-                onClick={() => setFilter(e.name)}
+                className={
+                  activeIndex === index
+                    ? `${classes.categoriesItem} ${classes.active}`
+                    : classes.categoriesItem
+                }
+                onClick={() => {
+                  setActiveIndex(index);
+                  setFilter(e.name);
+                }}
                 key={Math.random()}
               >
                 {e.name}
